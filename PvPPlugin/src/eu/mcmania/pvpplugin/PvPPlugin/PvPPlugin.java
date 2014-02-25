99178775b0a7e7fefcecf4abab68bcc7a2727a7b
Dark1ine <iamthedarkline@gmail.com>
2014-02-25 18:24:33 +0000

    Test
    Added a comment to test updating when pushing and pulling.

--- PvPPlugin/src/eu/mcmania/pvpplugin/PvPPlugin/PvPPlugin.java
+++ PvPPlugin/src/eu/mcmania/pvpplugin/PvPPlugin/PvPPlugin.java
@@ -16,7 +16,7 @@ public class PvPPlugin extends JavaPlugin {
 	
 	public void onEnable() {
 		getLogger().info("PvP Plugin booting up.");
-		this.saveDefaultConfig();
+		this.saveDefaultConfig(); //Creates default config.
 	}
 	public void onDisable() {
 		getLogger().info("PvP Plugin shutting down.");
