6226feb327f5e0d9b47e16010b752c79c007d04e
Lachlan <iamthedarkline@gmail.com>
2014-03-02 15:26:26 +0000

    Fixes
    Fixed most of errors that came up in Eclipse.

--- PvPPlugin/src/eu/mcmania/pvpplugin/PvPPlugin/StarterCMD.java
+++ PvPPlugin/src/eu/mcmania/pvpplugin/PvPPlugin/StarterCMD.java
@@ -14,71 +14,73 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.ChatColor
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.ChatColor;
 
 public class StarterCMD extends PvPPlugin {
-		
+
 	public void onRegionEnter(RegionEnterEvent e) {
-		
-		if (e.getPlayer().!hasPermission("pvp.starter") {
+		final Player player = e.getPlayer();
+
+		if (e.getPlayer().hasPermission("pvp.starter")) {
+
+			if (e.getRegion().getId().equals("starter")) {
+				final String InventoryToString(Inventory invInventory) {
+					String serialization = invInventory.getSize() + ";";
+					for (int i = 0; i < invInventory.getSize(); i++)
+					{
+						ItemStack is = invInventory.getItem(i);
+						if (is != null)
+						{
+							String serializedItemStack = new String();
+
+							String isType = String.valueOf(is.getType().getId());
+							serializedItemStack += "t@" + isType;
+
+							if (is.getDurability() != 0)
+							{
+								String isDurability = String.valueOf(is.getDurability());
+								serializedItemStack += ":d@" + isDurability;
+							}
+
+							if (is.getAmount() != 1)
+							{
+								String isAmount = String.valueOf(is.getAmount());
+								serializedItemStack += ":a@" + isAmount;
+							}
+
+							Map<Enchantment,Integer> isEnch = is.getEnchantments();
+							if (isEnch.size() > 0)
+							{
+								for (Entry<Enchantment,Integer> ench : isEnch.entrySet())
+								{
+									serializedItemStack += ":e@" + ench.getKey().getId() + "@" + ench.getValue();
+								}
+							}
+
+							serialization += i + "#" + serializedItemStack + ";";
 			
-			Player player = e.getPlayer();
-			Command cmd = "/spawn";
-			Bukkit.getServer().dispatchCommand(player.getServer().getConsoleSender(), cmd + player);
-			player.sendMessage(COLOR.RED + "You are not permitted to enter the starter arena.")
 		} else {
-		
-		if (e.getRegion().getId().equals("starter")) {
-		    public static String InventoryToString(Inventory invInventory) {
-		        String serialization = invInventory.getSize() + ";";
-		        for (int i = 0; i < invInventory.getSize(); i++)
-		        {
-		            ItemStack is = invInventory.getItem(i);
-		            if (is != null)
-		            {
-		                String serializedItemStack = new String();
-		               
-		                String isType = String.valueOf(is.getType().getId());
-		                serializedItemStack += "t@" + isType;
-		               
-		                if (is.getDurability() != 0)
-		                {
-		                    String isDurability = String.valueOf(is.getDurability());
-		                    serializedItemStack += ":d@" + isDurability;
-		                }
-		               
-		                if (is.getAmount() != 1)
-		                {
-		                    String isAmount = String.valueOf(is.getAmount());
-		                    serializedItemStack += ":a@" + isAmount;
-		                }
-		               
-		                Map<Enchantment,Integer> isEnch = is.getEnchantments();
-		                if (isEnch.size() > 0)
-		                {
-		                    for (Entry<Enchantment,Integer> ench : isEnch.entrySet())
-		                    {
-		                        serializedItemStack += ":e@" + ench.getKey().getId() + "@" + ench.getValue();
-		                    }
-		                }
-		               
-		                serialization += i + "#" + serializedItemStack + ";";
-		            }
-		        }
+			
+			String cmd = "/spawn";
+			Bukkit.getServer().dispatchCommand(player.getServer().getConsoleSender(), cmd + player);
+			player.sendMessage("&4You are not permitted to enter the starter arena.");
 
-		    }
+						}
+					}
 
-		}
-		
-		Player player = e.getPlayer();
-		ItemStack sword = new ItemStack((this.getConfig().getString(sword)));
-		ItemStack food = new ItemStack(this.getConfig().getString(food));
-		PlayerInventory inventory = player.getInventory();
-		
-		inventory.addItem(sword);
-		inventory.addItem(food);
-		
+				}
+
+			ItemStack sword = new ItemStack((this.getConfig().getString(sword)));
+			ItemStack food = new ItemStack(this.getConfig().getString(food));
+			PlayerInventory inventory = player.getInventory();
+
+			inventory.addItem(sword);
+			inventory.addItem(food);
+
+		} 
 	}
 
 }
 }
+
